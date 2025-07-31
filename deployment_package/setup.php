144784e6be18592ec27ee0193be2216c1f347ee7
softpro-ppm <softpro.ppm@gmail.com>
2025-07-31 09:54:24 +0530

    Update setup.php

--- deployment_package/setup.php
+++ deployment_package/setup.php
@@ -119,7 +119,7 @@ if ($agent_result) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Insurance Management System - Database Setup</title>
+    <title>Insurance Management System - Database Setup123</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
